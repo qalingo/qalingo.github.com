1e4cc10abb389f2d47b2775654965df510d2cbce
denis <denis@Hoteia-Laptop>
2014-07-14 09:59:20 +0200

    form validation

--- challenge/2014/js/form.js
+++ challenge/2014/js/form.js
@@ -3,26 +3,36 @@ $( document ).ready(function() {
 		var lastname = $('#lastname').val();
 		var firstname = $('#firstname').val();
 		var email = $('#email').val();
-		$.ajax({
-			url: "https://docs.google.com/a/hoteia.com/forms/d/16i7yyCCsapb_Be0QpJdPD6wHMNmdwZpINOA5MDeH7pM/formResponse",
-			data: { "entry.1464826012": $('#lastname').val(), "entry.1227480568": $('#firstname').val(), "entry.246834050": $('#email').val() },
-			type: "POST",
-			dataType: "xml",
-			success: function(data, textStatus, XMLHttpRequest) {
-				showConfirmationMessage();
-			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) {
-				showConfirmationMessage();
-			}
-		})
+		if(lastname!= '' && firstname!= '' && email!= ''){
+			$.ajax({
+				url: "https://docs.google.com/a/hoteia.com/forms/d/16i7yyCCsapb_Be0QpJdPD6wHMNmdwZpINOA5MDeH7pM/formResponse",
+				data: { "entry.1464826012": $('#lastname').val(), "entry.1227480568": $('#firstname').val(), "entry.246834050": $('#email').val() },
+				type: "POST",
+				dataType: "xml",
+				success: function(data, textStatus, XMLHttpRequest) {
+					showConfirmationMessage();
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown) {
+					showConfirmationMessage();
+				}
+			})
+		} else {
+			showErrorMessage();
+		}
 		return false;
 	});
 	
 	function showConfirmationMessage() {
 		$('#register-form-panel').hide();
+		$('#register-error-panel').hide();
 		$('#register-success-panel').show();
 	}
+	function showErrorMessage() {
+		$('#register-success-panel').hide();
+		$('#register-error-panel').show();
+	}
 	
+	$('#register-error-panel').hide();
 	$('#register-success-panel').hide();
 	
 });
